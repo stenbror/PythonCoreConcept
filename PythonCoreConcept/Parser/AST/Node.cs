adcde833aef8479e755b6648ec32193d635799c6
stenbror <stenbror@hotmail.com>
2021-02-24 23:51:51 +0100

    Started on AST for all atom nodes.

--- PythonCoreConcept/Parser/AST/Node.cs
+++ PythonCoreConcept/Parser/AST/Node.cs
@@ -8,7 +8,29 @@ namespace PythonCoreConcept.Parser.AST
 
     public record AtomFalse(UInt32 StartPos, UInt32 EndPos, Token Symbol) : ExpressionNode(StartPos, EndPos);
     
+    public record AtomTrue(UInt32 StartPos, UInt32 EndPos, Token Symbol) : ExpressionNode(StartPos, EndPos);
     
+    public record AtomNone(UInt32 StartPos, UInt32 EndPos, Token Symbol) : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomElipsis(UInt32 StartPos, UInt32 EndPos, Token Symbol) : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomName(UInt32 StartPos, UInt32 EndPos, Token Symbol) : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomNumber(UInt32 StartPos, UInt32 EndPos, Token Symbol) : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomString(UInt32 StartPos, UInt32 EndPos, Token[] Symbol) : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomTuple(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Right, Token Symbol2) 
+        : ExpressionNode(StartPos, EndPos);
+   
+    public record AtomList(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Right, Token Symbol2) 
+        : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomDictionary(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Right, Token Symbol2) 
+        : ExpressionNode(StartPos, EndPos);
+    
+    public record AtomSet(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Right, Token Symbol2) 
+        : ExpressionNode(StartPos, EndPos);
     
     public record StatementNode(UInt32 StartPos, UInt32 EndPos) : Node(StartPos, EndPos);
 }
