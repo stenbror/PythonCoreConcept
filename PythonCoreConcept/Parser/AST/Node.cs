fb06ff38eb76cdaf73b692e902bb3b3f52b95c27
stenbror <stenbror@hotmail.com>
2021-02-25 22:03:18 +0100

    Fixed inheritance bug in statement nodes.

--- PythonCoreConcept/Parser/AST/Node.cs
+++ PythonCoreConcept/Parser/AST/Node.cs
@@ -205,130 +205,166 @@ namespace PythonCoreConcept.Parser.AST
     public record IfStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right,
             StatementNode[] Nodes, StatementNode Next) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ElifStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ElseStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, Token Symbol2, StatementNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record WhileStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, ExpressionNode Left, Token Symbol2, StatementNode Right, StatementNode Next) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ForStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right, Token Symbol3, Token Symbol4,
             StatementNode Next, StatementNode Extra) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record TryStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, Token Symbol2, StatementNode Left,
             StatementNode[] ExceptNodes, StatementNode ElseNode, 
             Token Symbol3, Token Symbol4, StatementNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ExceptStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, ExpressionNode Left, Token Symbol2, Token Symbol3) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record WithStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, StatementNode[] WithItems, Token[] Separators, Token Symbol2, Token Symbol3,
             StatementNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record WithItemStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record SuiteStatement(UInt32 StartPos, UInt32 EndPos,
             Token Symbol1, Token Symbol2, StatementNode[] Nodes, Token Symbol3) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record SimpleStatement(UInt32 StartPos, UInt32 EndPos, StatementNode[] Nodes, Token Separators, Token Symbol) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record PlusAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record MinusAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record MulAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record MatriceAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record DivAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ModuloAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record BitAndAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record BitOrAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record BitXorAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ShiftLeftAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record ShiftRightAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record PowerAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record FloorDivAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol, ExpressionNode Right) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record AnnAssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token Symbol1, ExpressionNode Right, Token Symbol2, Node Next) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record AssignStatement(UInt32 StartPos, UInt32 EndPos, Node Left, Token[] Symbols, Node[] RightNodes, Token Symbol) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
     public record TestListStarExprStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode[] Nodes, Token[] Separators) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
-    public record DelStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, ExpressionNode Right) : Node(StartPos, EndPos);
+    public record DelStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, ExpressionNode Right) : StatementNode(StartPos, EndPos);
     
-    public record PassStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol) : Node(StartPos, EndPos);
+    public record PassStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol) : StatementNode(StartPos, EndPos);
     
-    public record BreakStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol) : Node(StartPos, EndPos);
+    public record BreakStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol) : StatementNode(StartPos, EndPos);
     
-    public record ContinueStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol) : Node(StartPos, EndPos);
+    public record ContinueStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol) : StatementNode(StartPos, EndPos);
     
-    public record ReturnStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, StatementNode Right) : Node(StartPos, EndPos);
+    public record ReturnStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, StatementNode Right) : StatementNode(StartPos, EndPos);
     
-    public record YieldStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode Right) : Node(StartPos, EndPos);
+    public record YieldStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode Right) : StatementNode(StartPos, EndPos);
     
-    public record RaiseStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right) : Node(StartPos, EndPos);
+    public record RaiseStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right) : StatementNode(StartPos, EndPos);
     
-    public record ImportNameStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, StatementNode Right) : Node(StartPos, EndPos);
+    public record ImportNameStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, StatementNode Right) : StatementNode(StartPos, EndPos);
     
     public record ImportFromStatement(UInt32 StartPos, UInt32 EndPos, 
         Token Symbol1, Token[] Dots, StatementNode Left, Token Symbol2, Token Symbol3, StatementNode Right, Token Symbol4) 
-        : Node(StartPos, EndPos);
+        : StatementNode(StartPos, EndPos);
     
-    public record ImportAsNameStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, Token Symbol2, Token Symbol3) : Node(StartPos, EndPos);
+    public record ImportAsNameStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, Token Symbol2, Token Symbol3) : StatementNode(StartPos, EndPos);
     
-    public record DottedAsNameStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode Left, Token Symbol1, Token Symbol2) : Node(StartPos, EndPos);
+    public record DottedAsNameStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode Left, Token Symbol1, Token Symbol2) : StatementNode(StartPos, EndPos);
     
-    public record ImportAsNamesStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode[] Nodes, Token[] Separators ) : Node(StartPos, EndPos);
+    public record ImportAsNamesStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode[] Nodes, Token[] Separators ) : StatementNode(StartPos, EndPos);
     
-    public record DottedAsNamesStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode[] Nodes, Token[] Separators) : Node(StartPos, EndPos);
+    public record DottedAsNamesStatement(UInt32 StartPos, UInt32 EndPos, ExpressionNode[] Nodes, Token[] Separators) : StatementNode(StartPos, EndPos);
     
-    public record DottedNameStatement(UInt32 StartPos, UInt32 EndPos, Token[] Nodes, Token[] Separators) : Node(StartPos, EndPos);
+    public record DottedNameStatement(UInt32 StartPos, UInt32 EndPos, Token[] Nodes, Token[] Separators) : StatementNode(StartPos, EndPos);
     
-    public record GlobalStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, Token[] Nodes, Token[] Separators) : Node(StartPos, EndPos);
+    public record GlobalStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, Token[] Nodes, Token[] Separators) : StatementNode(StartPos, EndPos);
     
-    public record NonlocalStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, Token[] Nodes, Token[] Separators) : Node(StartPos, EndPos);
+    public record NonlocalStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, Token[] Nodes, Token[] Separators) : StatementNode(StartPos, EndPos);
     
-    public record AssertStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right) : Node(StartPos, EndPos);
+    public record AssertStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, ExpressionNode Left, Token Symbol2, ExpressionNode Right) 
+        : StatementNode(StartPos, EndPos);
+    
+    public record FuncBodySuiteStatement(UInt32 StartPos, UInt32 EndPos, 
+            Token Symbol1, Token Symbol2, Token Symbol3, Token Symbol4, StatementNode[] Nodes, Token Symbol5) 
+        : StatementNode(StartPos, EndPos);
+    
+    public record ClassStatement(UInt32 StartPos, UInt32 EndPos, 
+        Token Symbol1, Token Symbol2, Token Symbol3, ExpressionNode Left, Token Symbol4, Token Symbol5, StatementNode Right) : StatementNode(StartPos, EndPos);
+    
+    public record AsyncStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol, StatementNode Right) : StatementNode(StartPos, EndPos);
+    
+    public record DecoratorStatement(UInt32 StartPos, UInt32 EndPos, 
+        Token Symbol, StatementNode Left, Token Symbol2, ExpressionNode Right, Token Symbol3, Token Symbol4) : StatementNode(StartPos, EndPos);
+    
+    public record DecoratorsStatement(UInt32 StartPos, UInt32 EndPos, StatementNode[] Right) : StatementNode(StartPos, EndPos);
+    
+    public record DecoratedStatement(UInt32 StartPos, UInt32 EndPos, StatementNode Left, StatementNode Right) : StatementNode(StartPos, EndPos);
+    
+    public record FuncDefStatement(UInt32 StartPos, UInt32 EndPos, 
+        Token Symbol1, Token Symbol2, StatementNode Left, Token Symbol3, Token Symbol4, Token Symbol5, Token Symbol6, StatementNode Right) 
+        : StatementNode(StartPos, EndPos);
+    
+    public record ParametersStatement(UInt32 StartPos, UInt32 EndPos, Token Symbol1, StatementNode Right, Token Symbol2) : StatementNode(StartPos, EndPos);
+    
+    public record TypedArgsListStatement(UInt32 StartPos, UInt32 EndPos, 
+        StatementNode[] Nodes, Token[] Separators, Token Slash, Token Mul, ExpressionNode MulNode, Token Power, ExpressionNode PowerNode, Token TypeComment) 
+        : StatementNode(StartPos, EndPos);
+   
+    
+    
+    
+    
+    
+    public record VarArgsListStatement(UInt32 StartPos, UInt32 EndPos, 
+            StatementNode[] Nodes, Token[] Separators, Token Slash, Token Mul, ExpressionNode MulNode, Token Power, ExpressionNode PowerNode, Token TypeComment) 
+        : ExpressionNode(StartPos, EndPos);
 }
