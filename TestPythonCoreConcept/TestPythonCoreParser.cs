1d01bd4fb145408ca6063809165119a3b0cdfa97
stenbror <stenbror@hotmail.com>
2021-03-08 18:29:33 +0100

    Added UnitTests for Multiple Subscript.

--- TestPythonCoreConcept/TestPythonCoreParser.cs
+++ TestPythonCoreConcept/TestPythonCoreParser.cs
@@ -1105,6 +1105,33 @@ namespace TestPythonCoreConcept
                 Assert.Equal(TokenKind.PyRightBracket, node1.Symbol2.Kind);
             }
             
+            [Fact]
+            public void TestAtomNameIndex8()
+            {
+                var parser = new PythonCoreParser(new PythonCoreTokenizer("a[1:10, 2, 3, ]".ToArray()));
+                var rootNode = parser.ParseEvalInput();
+                Assert.True(rootNode is EvalInputNode);
+                Assert.Equal(TokenKind.EndOfFile, (rootNode as EvalInputNode).Eof.Kind);
+                Assert.True((rootNode as EvalInputNode).Newlines.Length == 0);
+                var node = (rootNode as EvalInputNode).Right;
+                Assert.True(node is AtomExpr);
+                var node0 = (node as AtomExpr);
+                Assert.Equal(0u, node0.StartPos);
+                Assert.Equal(15u, node0.EndPos);
+                Assert.True(node0.Left is AtomName);
+                Assert.True(node0.Right[0] is Index);
+                var node1 = (node0.Right[0] as Index);
+                Assert.Equal(TokenKind.PyLeftBracket, node1.Symbol1.Kind);
+                Assert.True(node1.Right is SubscriptList);
+                var node2 = (node1.Right as SubscriptList);
+                Assert.True(node2.Nodes.Length == 3);
+                Assert.True(node2.Separators.Length == 3);
+                Assert.True(node2.Nodes[0] is Subscript);
+                Assert.True(node2.Nodes[1] is Subscript);
+                Assert.True(node2.Nodes[2] is Subscript);
+                Assert.Equal(TokenKind.PyRightBracket, node1.Symbol2.Kind);
+            }
+            
             
         }
     }
