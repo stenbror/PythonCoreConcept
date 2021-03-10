737b94c0eb8c40a7eb0abbdc447d1f5ff5c39be5
stenbror <stenbror@hotmail.com>
2021-03-10 21:59:05 +0100

    Added Statement test harness.

---
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -0,0 +1,16 @@
+
+using Xunit;
+using PythonCoreConcept.Parser;
+using PythonCoreConcept.Parser.AST;
+
+namespace TestPythonCoreConcept
+{
+    public class TestPythonCoreParserStatement
+    {
+        [Fact]
+        public void EmptyTest()
+        {
+            
+        }
+    }
+}
