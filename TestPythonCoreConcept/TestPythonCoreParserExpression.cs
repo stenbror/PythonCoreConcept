836e8e637e6d1096ec3ec52e24c256a7ef4ab31a
stenbror <stenbror@hotmail.com>
2021-03-10 20:01:07 +0100

    Added UnitTest for lambda expression with arguments.

--- TestPythonCoreConcept/TestPythonCoreParserExpression.cs
+++ TestPythonCoreConcept/TestPythonCoreParserExpression.cs
@@ -2405,6 +2405,33 @@ namespace TestPythonCoreConcept
                 Assert.Equal(TokenKind.PyPower, node1.Power.Kind);
                 Assert.True(node1.PowerNode is NameToken);
             }
+            
+            [Fact]
+            public void TestLambdaParameters16()
+            {
+                var parser = new PythonCoreParser(new PythonCoreTokenizer("lambda *a, b, **c: x".ToCharArray()));
+                var rootNode = parser.ParseEvalInput();
+                Assert.True(rootNode is EvalInputNode);
+                Assert.Equal(TokenKind.EndOfFile, (rootNode as EvalInputNode).Eof.Kind);
+                Assert.True((rootNode as EvalInputNode).Newlines.Length == 0);
+                var node = (rootNode as EvalInputNode).Right;
+                Assert.True(node is Lambda);
+                var node0 = (node as Lambda);
+                Assert.Equal(0u, node0.StartPos);
+                Assert.Equal(20u, node0.EndPos);
+                Assert.Equal(TokenKind.PyLambda, node0.Symbol1.Kind);
+                Assert.Equal(TokenKind.PyColon, node0.Symbol2.Kind);
+                Assert.True(node0.Right is AtomName);
+                Assert.True(node0.Left is VarArgsListStatement);
+                var node1 = (node0.Left as VarArgsListStatement);
+                Assert.True(node1.Nodes.Length == 1);
+                Assert.True(node1.Separators.Length == 2);
+                Assert.True(node1.Slash == null);
+                Assert.Equal(TokenKind.PyMul, node1.Mul.Kind);
+                Assert.True(node1.MulNode is NameToken);
+                Assert.Equal(TokenKind.PyPower, node1.Power.Kind);
+                Assert.True(node1.PowerNode is NameToken);
+            }
         }
     }
 }
