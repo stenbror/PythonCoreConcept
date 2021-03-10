734c39b1b258a9c13c98837479124b5f831bb01c
stenbror <stenbror@hotmail.com>
2021-03-10 21:45:41 +0100

    Added UnitTest for FuncType.

--- TestPythonCoreConcept/TestPythonCoreParserFunctional.cs
+++ TestPythonCoreConcept/TestPythonCoreParserFunctional.cs
@@ -266,5 +266,31 @@ namespace TestPythonCoreConcept
             
             Assert.True(node0.Right is AtomName);
         }
+        
+        [Fact]
+        public void TestFuncInputWithParameters10()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("(*a) -> x".ToCharArray()));
+            var rootNode = parser.ParseFuncTypeInput();
+            Assert.True(rootNode is TypeInput);
+            var node = (rootNode as TypeInput);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.Equal(0u, node.StartPos);
+            Assert.Equal(9u, node.EndPos);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.True(node.Right is FuncType);
+            var node0 = (node.Right as FuncType);
+            Assert.Equal(TokenKind.PyLeftParen, node0.Symbol1.Kind);
+            Assert.Equal(TokenKind.PyRightParen, node0.Symbol2.Kind);
+            Assert.Equal(TokenKind.PyArrow, node0.Symbol3.Kind);
+            Assert.True(node0.Left is TypeList);
+            var node1 = (node0.Left as TypeList);
+            Assert.True(node1.Nodes.Length == 0);
+            Assert.True(node1.Separators.Length == 0);
+            Assert.Equal(TokenKind.PyMul, node1.Mul.Kind);
+            Assert.True(node1.MulNode is AtomName);
+            
+            Assert.True(node0.Right is AtomName);
+        }
     }
 }
