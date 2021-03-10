5739a4bdb94ff95bd79d445a244dc5a7dcc194cf
stenbror <stenbror@hotmail.com>
2021-03-10 21:42:40 +0100

    Added UnitTest for FuncType.

--- TestPythonCoreConcept/TestPythonCoreParserFunctional.cs
+++ TestPythonCoreConcept/TestPythonCoreParserFunctional.cs
@@ -187,5 +187,84 @@ namespace TestPythonCoreConcept
             Assert.True(node0.Right is AtomName);
         }
         
+        [Fact]
+        public void TestFuncInputWithParameters7()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("(**f) -> x".ToCharArray()));
+            var rootNode = parser.ParseFuncTypeInput();
+            Assert.True(rootNode is TypeInput);
+            var node = (rootNode as TypeInput);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.Equal(0u, node.StartPos);
+            Assert.Equal(10u, node.EndPos);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.True(node.Right is FuncType);
+            var node0 = (node.Right as FuncType);
+            Assert.Equal(TokenKind.PyLeftParen, node0.Symbol1.Kind);
+            Assert.Equal(TokenKind.PyRightParen, node0.Symbol2.Kind);
+            Assert.Equal(TokenKind.PyArrow, node0.Symbol3.Kind);
+            Assert.True(node0.Left is TypeList);
+            var node1 = (node0.Left as TypeList);
+            Assert.True(node1.Nodes.Length == 0);
+            Assert.True(node1.Separators.Length == 0);
+            Assert.Equal(TokenKind.PyPower, node1.Power.Kind);
+            Assert.True(node1.PowerNode is AtomName);
+
+            Assert.True(node0.Right is AtomName);
+        }
+        
+        [Fact]
+        public void TestFuncInputWithParameters8()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("(*a, b, c, **d) -> x".ToCharArray()));
+            var rootNode = parser.ParseFuncTypeInput();
+            Assert.True(rootNode is TypeInput);
+            var node = (rootNode as TypeInput);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.Equal(0u, node.StartPos);
+            Assert.Equal(20u, node.EndPos);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.True(node.Right is FuncType);
+            var node0 = (node.Right as FuncType);
+            Assert.Equal(TokenKind.PyLeftParen, node0.Symbol1.Kind);
+            Assert.Equal(TokenKind.PyRightParen, node0.Symbol2.Kind);
+            Assert.Equal(TokenKind.PyArrow, node0.Symbol3.Kind);
+            Assert.True(node0.Left is TypeList);
+            var node1 = (node0.Left as TypeList);
+            Assert.True(node1.Nodes.Length == 2);
+            Assert.True(node1.Separators.Length == 3);
+            Assert.Equal(TokenKind.PyMul, node1.Mul.Kind);
+            Assert.True(node1.MulNode is AtomName);
+            Assert.Equal(TokenKind.PyPower, node1.Power.Kind);
+            Assert.True(node1.PowerNode is AtomName);
+
+            Assert.True(node0.Right is AtomName);
+        }
+        
+        [Fact]
+        public void TestFuncInputWithParameters9()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("(*a, b, c) -> x".ToCharArray()));
+            var rootNode = parser.ParseFuncTypeInput();
+            Assert.True(rootNode is TypeInput);
+            var node = (rootNode as TypeInput);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.Equal(0u, node.StartPos);
+            Assert.Equal(15u, node.EndPos);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.True(node.Right is FuncType);
+            var node0 = (node.Right as FuncType);
+            Assert.Equal(TokenKind.PyLeftParen, node0.Symbol1.Kind);
+            Assert.Equal(TokenKind.PyRightParen, node0.Symbol2.Kind);
+            Assert.Equal(TokenKind.PyArrow, node0.Symbol3.Kind);
+            Assert.True(node0.Left is TypeList);
+            var node1 = (node0.Left as TypeList);
+            Assert.True(node1.Nodes.Length == 2);
+            Assert.True(node1.Separators.Length == 2);
+            Assert.Equal(TokenKind.PyMul, node1.Mul.Kind);
+            Assert.True(node1.MulNode is AtomName);
+            
+            Assert.True(node0.Right is AtomName);
+        }
     }
 }
