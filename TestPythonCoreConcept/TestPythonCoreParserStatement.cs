f9a1d616a4dd84e120ff1c96732db4f79f4d6ab1
stenbror <stenbror@hotmail.com>
2021-03-11 17:07:00 +0100

    Added UnitTest for pass statement.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -46,5 +46,33 @@ namespace TestPythonCoreConcept
             var node1 = (node0.Nodes[0] as PassStatement);
             Assert.Equal(TokenKind.PyPass, node1.Symbol.Kind);
         }
+        
+        [Fact]
+        public void TestMultipleSimpleStmt()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("pass;pass\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 2);
+            Assert.True(node0.Separators.Length == 1);
+            Assert.True(node0.Nodes[0] is PassStatement);
+            var node1 = (node0.Nodes[0] as PassStatement);
+            Assert.Equal(TokenKind.PyPass, node1.Symbol.Kind);
+            Assert.True(node1.StartPos == 0u);
+            Assert.True(node1.EndPos == 4u);
+            Assert.True(node0.Nodes[1] is PassStatement);
+            var node2 = (node0.Nodes[1] as PassStatement);
+            Assert.Equal(TokenKind.PyPass, node2.Symbol.Kind);
+            Assert.True(node2.StartPos == 5u);
+            Assert.True(node2.EndPos == 9u);
+
+        }
     }
 }
