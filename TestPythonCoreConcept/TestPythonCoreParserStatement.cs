61f772df7c5eef8330bb15ae8f77d4e8c2e56257
stenbror <stenbror@hotmail.com>
2021-03-11 20:53:38 +0100

    Added UnitTest for MulAssign statement.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -169,5 +169,33 @@ namespace TestPythonCoreConcept
             Assert.True(node2.Nodes[0] is AtomName);
             Assert.True(node1.Right is AtomNumber);
         }
+        
+        [Fact]
+        public void TestMulAssignStmt()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("a *= 1\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 1);
+            Assert.True(node0.Separators.Length == 0);
+            Assert.True(node0.Nodes[0] is MulAssignStatement);
+            var node1 = (node0.Nodes[0] as MulAssignStatement);
+            Assert.Equal(TokenKind.PyMulAssign, node1.Symbol.Kind);
+            Assert.True(node1.StartPos == 0u);
+            Assert.True(node1.EndPos == 6u);
+            Assert.True(node1.Left is TestListStarExprStatement);
+            var node2 = (node1.Left as TestListStarExprStatement);
+            Assert.True(node2.Nodes.Length == 1);
+            Assert.True(node2.Separators.Length == 0);
+            Assert.True(node2.Nodes[0] is AtomName);
+            Assert.True(node1.Right is AtomNumber);
+        }
     }
 }
