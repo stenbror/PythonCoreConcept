938db10e734e2fea8ccd6e5dbcd3a7b013783813
stenbror <stenbror@hotmail.com>
2021-03-11 20:55:20 +0100

    Added UnitTest for DivAssign statement.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -197,5 +197,33 @@ namespace TestPythonCoreConcept
             Assert.True(node2.Nodes[0] is AtomName);
             Assert.True(node1.Right is AtomNumber);
         }
+        
+        [Fact]
+        public void TestDivAssignStmt()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("a /= 1\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 1);
+            Assert.True(node0.Separators.Length == 0);
+            Assert.True(node0.Nodes[0] is DivAssignStatement);
+            var node1 = (node0.Nodes[0] as DivAssignStatement);
+            Assert.Equal(TokenKind.PyDivAssign, node1.Symbol.Kind);
+            Assert.True(node1.StartPos == 0u);
+            Assert.True(node1.EndPos == 6u);
+            Assert.True(node1.Left is TestListStarExprStatement);
+            var node2 = (node1.Left as TestListStarExprStatement);
+            Assert.True(node2.Nodes.Length == 1);
+            Assert.True(node2.Separators.Length == 0);
+            Assert.True(node2.Nodes[0] is AtomName);
+            Assert.True(node1.Right is AtomNumber);
+        }
     }
 }
