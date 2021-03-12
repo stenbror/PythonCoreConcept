af87a920bad698c5d4eea7413d7fb34095be92a5
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-12 22:41:12 +0100

    Added UnitTest for Single AnnAssign with Right side of assign.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -631,5 +631,35 @@ namespace TestPythonCoreConcept
             Assert.True(node1.Right is AtomName);
             Assert.True(node1.Next == null);
         }
+        
+        [Fact]
+        public void TestSingleAnnAssign()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("*a : int = 1\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 1);
+            Assert.True(node0.Separators.Length == 0);
+            Assert.True(node0.Nodes[0] is AnnAssignStatement);
+            var node1 = (node0.Nodes[0] as AnnAssignStatement);
+            Assert.Equal(TokenKind.PyColon, node1.Symbol1.Kind);
+            Assert.True(node1.StartPos == 0u);
+            Assert.True(node1.EndPos == 12u);
+            Assert.True(node1.Left is TestListStarExprStatement);
+            var node2 = (node1.Left as TestListStarExprStatement);
+            Assert.True(node2.Nodes.Length == 1);
+            Assert.True(node2.Separators.Length == 0);
+            Assert.True(node2.Nodes[0] is StarExpr);
+            Assert.True(node1.Right is AtomName);
+            Assert.True(node1.Next is TestListStarExprStatement);
+            Assert.Equal(TokenKind.PyAssign, node1.Symbol2.Kind);
+        }
     }
 }
