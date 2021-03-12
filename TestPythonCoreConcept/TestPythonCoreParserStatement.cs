62867dc275ecec562715262661d25a35970e29ae
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-12 23:34:02 +0100

    Added UnitTest for global statement with multiple argument.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -768,5 +768,34 @@ namespace TestPythonCoreConcept
             Assert.True(node1.Separators.Length == 0);
             Assert.Equal(TokenKind.Name, node1.Nodes[0].Kind);
         }
+        
+        [Fact]
+        public void TestGlobalStatementMultipleArguments()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("global a, b, c\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 1);
+            Assert.True(node0.Separators.Length == 0);
+            Assert.True(node0.Nodes[0] is GlobalStatement);
+            var node1 = (node0.Nodes[0] as GlobalStatement);
+            Assert.Equal(TokenKind.PyGlobal, node1.Symbol.Kind);
+            Assert.Equal(0u, node1.StartPos);
+            Assert.Equal(14u, node1.EndPos);
+            Assert.True(node1.Nodes.Length == 3);
+            Assert.True(node1.Separators.Length == 2);
+            Assert.Equal(TokenKind.Name, node1.Nodes[0].Kind);
+            Assert.Equal(TokenKind.Name, node1.Nodes[1].Kind);
+            Assert.Equal(TokenKind.Name, node1.Nodes[2].Kind);
+            Assert.Equal(TokenKind.PyComma, node1.Separators[0].Kind);
+            Assert.Equal(TokenKind.PyComma, node1.Separators[1].Kind);
+        }
     }
 }
