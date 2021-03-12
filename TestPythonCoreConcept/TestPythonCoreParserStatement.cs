f0c24c7a7a6d3047244755add3ac80fdd44b150f
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-12 23:15:44 +0100

    Added UnitTest for Del statement with single argument.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -691,5 +691,28 @@ namespace TestPythonCoreConcept
             Assert.True(node1.Next is YieldExpr);
             Assert.Equal(TokenKind.PyAssign, node1.Symbol2.Kind);
         }
+        
+        [Fact]
+        public void TestDelStatement()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("del a\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 1);
+            Assert.True(node0.Separators.Length == 0);
+            Assert.True(node0.Nodes[0] is DelStatement);
+            var node1 = (node0.Nodes[0] as DelStatement);
+            Assert.Equal(TokenKind.PyDel, node1.Symbol.Kind);
+            Assert.Equal(0u, node1.StartPos);
+            Assert.Equal(5u, node1.EndPos);
+            Assert.True(node1.Right is AtomName);
+        }
     }
 }
