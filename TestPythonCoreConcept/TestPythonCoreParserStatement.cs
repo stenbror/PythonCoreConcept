67dbd9ae36871ee0f30767659bc87f15e193ec1b
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-13 14:06:10 +0100

    Added UnitTest for import statement.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -994,5 +994,57 @@ namespace TestPythonCoreConcept
             Assert.Equal(TokenKind.PyDot, node2.Separators[0].Kind);
             Assert.Equal(TokenKind.PyDot, node2.Separators[1].Kind);
         }
+        
+        [Fact]
+        public void TestImportMultipleDottedAsStatement()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("import a.b.c as d, e.f.g as h\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is SimpleStatement);
+            var node0 = (node.Nodes[0] as SimpleStatement);
+            Assert.Equal(TokenKind.Newline, node0.Symbol.Kind);
+            Assert.True(node0.Nodes.Length == 1);
+            Assert.True(node0.Separators.Length == 0);
+            Assert.True(node0.Nodes[0] is ImportNameStatement);
+            var node1 = (node0.Nodes[0] as ImportNameStatement);
+            Assert.Equal(TokenKind.PyImport, node1.Symbol.Kind);
+            Assert.Equal(0u, node1.StartPos);
+            Assert.Equal(29u, node1.EndPos);
+            Assert.True(node1.Right is DottedAsNamesStatement);
+            var node10 = (node1.Right as DottedAsNamesStatement);
+            Assert.True(node10.Nodes.Length == 2);
+            Assert.True(node10.Separators.Length == 1);
+            
+            var node3 = (node10.Nodes[0] as DottedAsNameStatement);
+            Assert.True(node3.Left is DottedNameStatement);
+            Assert.Equal(TokenKind.PyAs, node3.Symbol1.Kind);
+            Assert.Equal(TokenKind.Name, node3.Symbol2.Kind);
+            var node2 = (node3.Left as DottedNameStatement);
+            Assert.True(node2.Nodes.Length == 3);
+            Assert.True(node2.Separators.Length == 2);
+            Assert.Equal(TokenKind.Name, node2.Nodes[0].Kind);
+            Assert.Equal(TokenKind.Name, node2.Nodes[1].Kind);
+            Assert.Equal(TokenKind.Name, node2.Nodes[2].Kind);
+            Assert.Equal(TokenKind.PyDot, node2.Separators[0].Kind);
+            Assert.Equal(TokenKind.PyDot, node2.Separators[1].Kind);
+            
+            var node4 = (node10.Nodes[1] as DottedAsNameStatement);
+            Assert.True(node4.Left is DottedNameStatement);
+            Assert.Equal(TokenKind.PyAs, node4.Symbol1.Kind);
+            Assert.Equal(TokenKind.Name, node4.Symbol2.Kind);
+            var node5 = (node4.Left as DottedNameStatement);
+            Assert.True(node5.Nodes.Length == 3);
+            Assert.True(node5.Separators.Length == 2);
+            Assert.Equal(TokenKind.Name, node5.Nodes[0].Kind);
+            Assert.Equal(TokenKind.Name, node5.Nodes[1].Kind);
+            Assert.Equal(TokenKind.Name, node5.Nodes[2].Kind);
+            Assert.Equal(TokenKind.PyDot, node5.Separators[0].Kind);
+            Assert.Equal(TokenKind.PyDot, node5.Separators[1].Kind);
+        }
     }
 }
