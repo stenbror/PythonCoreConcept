37f4653e32e9f50736d2bb81864da6fcbf343beb
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-14 18:03:33 +0100

    Added UnitTest for func def statement.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -1965,5 +1965,30 @@ namespace TestPythonCoreConcept
             Assert.Equal(TokenKind.PyColon, node3.Symbol5.Kind);
             Assert.True(node3.Next is SimpleStatement);
         }
+        
+        [Fact]
+        public void TestCompoundSimpleAsyncDefReturnStatement()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("async def a() -> b: break\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is AsyncStatement);
+            var node1 = (node.Nodes[0] as AsyncStatement);
+            Assert.Equal(0u, node1.StartPos);
+            Assert.Equal(26u, node1.EndPos);
+            Assert.Equal(TokenKind.PyAsync, node1.Symbol.Kind);
+            Assert.True(node1.Right is FuncDefStatement);
+            var node3 = (node1.Right as FuncDefStatement);
+            Assert.Equal(TokenKind.PyDef, node3.Symbol1.Kind);
+            Assert.Equal(TokenKind.Name, node3.Symbol2.Kind);
+            Assert.Equal(TokenKind.PyArrow, node3.Symbol3.Kind);
+            Assert.Equal(TokenKind.PyColon, node3.Symbol5.Kind);
+            Assert.True(node3.Right is AtomName);
+            Assert.True(node3.Next is SimpleStatement);
+        }
     }
 }
