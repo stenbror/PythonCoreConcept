6e0e63020287e8c4fce150be1489ba208b262121
stenbror <stenbror@hotmail.com>
2021-03-17 21:29:15 +0100

    Added UnitTest for class declarartion.

--- TestPythonCoreConcept/TestPythonCoreParserStatement.cs
+++ TestPythonCoreConcept/TestPythonCoreParserStatement.cs
@@ -2449,5 +2449,26 @@ namespace TestPythonCoreConcept
             Assert.True(node1.Left == null);
             Assert.True(node1.Right is SimpleStatement);
         }
+        
+        [Fact]
+        public void TestCompoundClassStatementEmpty()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("class a: pass\n".ToCharArray()));
+            var rootNode = parser.ParseFileInput();
+            Assert.True(rootNode is FileInputNode);
+            var node = (rootNode as FileInputNode);
+            Assert.True(node.Newlines.Length == 0);
+            Assert.Equal(TokenKind.EndOfFile, node.Eof.Kind);
+            Assert.True(node.Nodes.Length == 1);
+            Assert.True(node.Nodes[0] is ClassStatement);
+            var node1 = (node.Nodes[0] as ClassStatement);
+            Assert.Equal(0u, node1.StartPos);
+            Assert.Equal(14u, node1.EndPos);
+            Assert.Equal(TokenKind.PyClass, node1.Symbol1.Kind);
+            Assert.Equal(TokenKind.Name, node1.Symbol2.Kind);
+            Assert.Equal(TokenKind.PyColon, node1.Symbol5.Kind);
+            Assert.True(node1.Left == null);
+            Assert.True(node1.Right is SimpleStatement);
+        }
     }
 }
