bbe1539940c69159cf0b2c3fa16eef3aed21dec6
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-21 11:31:55 +0100

    Added Entry for SingleInput parsing.

--- PythonCoreConcept/PythonCore.cs
+++ PythonCoreConcept/PythonCore.cs
@@ -29,5 +29,13 @@ namespace PythonCoreConcept
           var parser = new PythonCoreParser(lex);
           return parser.ParseFileInput();
         }
+
+        public static Node ParseSingleInput(string sourceCode, UInt32 tabSize = 8u)
+        {
+            var lex = new PythonCoreTokenizer(sourceCode.ToCharArray());
+            lex.TabSize = tabSize;
+            var parser = new PythonCoreParser(lex);
+            return parser.ParseSingleInput();
+        }
     }
 }
