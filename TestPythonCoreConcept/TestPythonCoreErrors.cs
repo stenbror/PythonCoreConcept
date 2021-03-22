bc04dfaa66e9f4cbbcb7b4a9cee35e3d38f908ac
stenbror <stenbror@hotmail.com>
2021-03-22 21:49:05 +0100

    Added UnitTest for SyntaxError in trailer rule.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -70,5 +70,47 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestAtomDictionaryError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("{a\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(2u, e.Position);
+                Assert.Equal("Missing '}' in literal!", e.Message);
+                Assert.Equal(TokenKind.Newline, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
+        
+        [Fact]
+        public void TestTrailerCallError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("a(b\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(3u, e.Position);
+                Assert.Equal("Expecting ')' in call!", e.Message);
+                Assert.Equal(TokenKind.Newline, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
