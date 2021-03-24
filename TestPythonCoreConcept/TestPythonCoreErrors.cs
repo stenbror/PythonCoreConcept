dcc55cecb63acd011c66749c090a45b73c52479a
stenbror <stenbror@hotmail.com>
2021-03-24 19:41:04 +0100

    Added UnitTest for SyntaxError in lambda rule.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -175,5 +175,26 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestLambdaError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("lambda a pass\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(9u, e.Position);
+                Assert.Equal("Missing ':' in lambda expression!", e.Message);
+                Assert.Equal(TokenKind.PyPass, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
