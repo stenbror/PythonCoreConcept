02d01a26d13cf1bc4d2c25048068fcb96b32d411
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-28 22:55:47 +0200

    Added UnitTest for FileInput. Except missing colon.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -679,5 +679,26 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestFileInputCompundExceptMissingColonError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("try: pass\nexcept pass\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(17u, e.Position);
+                Assert.Equal("Illegal literal!", e.Message);
+                Assert.Equal(TokenKind.PyPass, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
