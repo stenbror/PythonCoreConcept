ed2771e231da8c0a8badccd7ae7775e4c14e7a7c
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-28 14:49:00 +0200

    Added UnitTest for FuncTypeInput.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -406,5 +406,26 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestFuncTypeMissingParenthesisError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("( -> a\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFuncTypeInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(2u, e.Position);
+                Assert.Equal("Illegal literal!", e.Message);
+                Assert.Equal(TokenKind.PyArrow, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
