345b4e25ccd088d3a99c9bd633a301ca9eee5bc1
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-29 17:50:19 +0200

    Added UnitTest for FileInput. Class argument missing after mul.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -826,5 +826,26 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestFileInputCompundClassMissingItemAfterMulError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("class a(*) pass\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(9u, e.Position);
+                Assert.Equal("Illegal literal!", e.Message);
+                Assert.Equal(TokenKind.PyRightParen, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
