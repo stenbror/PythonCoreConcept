c6b1d10cd829278809e8031e653bfc8bc6839ecc
Richard Magnor Stenbro <stenbror@hotmail.com>
2021-03-30 00:03:39 +0200

    Added UnitTest for func def with missing after arrow.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -1078,5 +1078,26 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestFileInputCompundFuncDefMissingAfterArrowError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("def a() -> : pass\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(11u, e.Position);
+                Assert.Equal("Illegal literal!", e.Message);
+                Assert.Equal(TokenKind.PyColon, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
