054616de950cb8c64726faedd1009a348a29ba44
stenbror <stenbror@hotmail.com>
2021-04-02 01:12:28 +0200

    Added UnitTest for assert statement.

--- TestPythonCoreConcept/TestPythonCoreErrors.cs
+++ TestPythonCoreConcept/TestPythonCoreErrors.cs
@@ -1561,5 +1561,26 @@ namespace TestPythonCoreConcept
                 Assert.True(false);
             }
         }
+        
+        [Fact]
+        public void TestFileInputAssertMissingAfterCommaError()
+        {
+            var parser = new PythonCoreParser(new PythonCoreTokenizer("assert a,;\n".ToCharArray()));
+            try
+            {
+                var rootNode = parser.ParseFileInput();
+                Assert.True(false);
+            }
+            catch (SyntaxError e)
+            {
+                Assert.Equal(9u, e.Position);
+                Assert.Equal("Illegal literal!", e.Message);
+                Assert.Equal(TokenKind.PySemiColon, e.Symbol.Kind);
+            }
+            catch 
+            {
+                Assert.True(false);
+            }
+        }
     }
 }
