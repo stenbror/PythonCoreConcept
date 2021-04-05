1d44741b1d748a6b66f4eb74dd24f2bbbff3d785
stenbror <stenbror@hotmail.com>
2021-04-05 13:37:36 +0200

    Added UnitTest for import statement.

--- PythonCoreConcept/PythonCore.cs
+++ PythonCoreConcept/PythonCore.cs
@@ -6,34 +6,34 @@ namespace PythonCoreConcept
 {
     public class PythonFrontEnd
     {
-        public static Node ParseEvalInput(string sourceCode, UInt32 tabSize = 8u)
+        public static Node ParseEvalInput(string SourceCode, UInt32 TabSize = 8u)
         {
-            var lex = new PythonCoreTokenizer(sourceCode.ToCharArray());
-            lex.TabSize = tabSize;
+            var lex = new PythonCoreTokenizer(SourceCode.ToCharArray());
+            lex.TabSize = TabSize;
             var parser = new PythonCoreParser(lex);
             return parser.ParseEvalInput();
         }
         
-        public static Node ParseFuncTypeInput(string sourceCode, UInt32 tabSize = 8u)
+        public static Node ParseFuncTypeInput(string SourceCode, UInt32 TabSize = 8u)
         {
-          var lex = new PythonCoreTokenizer(sourceCode.ToCharArray());
-          lex.TabSize = tabSize;
+          var lex = new PythonCoreTokenizer(SourceCode.ToCharArray());
+          lex.TabSize = TabSize;
           var parser = new PythonCoreParser(lex);
           return parser.ParseFuncTypeInput();
         }
         
-        public static Node ParseFileInput(string sourceCode, UInt32 tabSize = 8u)
+        public static Node ParseFileInput(string SourceCode, UInt32 TabSize = 8u)
         {
-          var lex = new PythonCoreTokenizer(sourceCode.ToCharArray());
-          lex.TabSize = tabSize;
+          var lex = new PythonCoreTokenizer(SourceCode.ToCharArray());
+          lex.TabSize = TabSize;
           var parser = new PythonCoreParser(lex);
           return parser.ParseFileInput();
         }
 
-        public static Node ParseSingleInput(string sourceCode, UInt32 tabSize = 8u)
+        public static Node ParseSingleInput(string SourceCode, UInt32 TabSize = 8u)
         {
-            var lex = new PythonCoreTokenizer(sourceCode.ToCharArray());
-            lex.TabSize = tabSize;
+            var lex = new PythonCoreTokenizer(SourceCode.ToCharArray());
+            lex.TabSize = TabSize;
             var parser = new PythonCoreParser(lex);
             return parser.ParseSingleInput();
         }
